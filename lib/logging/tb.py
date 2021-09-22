05638a39bea8c3cafb045c93e804c8de22d4252a
Lucas Brynte <brynte@chalmers.se>
2021-09-22 16:03:41 +0200

    Various fixups

--- lib/logging/tb.py
+++ lib/logging/tb.py
@@ -1,4 +1,16 @@
 from torch.utils.tensorboard import SummaryWriter
 from lib.config.config import config
 
-tb_writer = SummaryWriter(config.tb_path)
+global tb_writer
+tb_writer = None
+
+def initialize_tensorboard():
+    global tb_writer
+    tb_writer = SummaryWriter(
+        log_dir = config.tb_path,
+        flush_secs = 30,
+    )
+    return tb_writer
+
+def get_tb_writer():
+    return tb_writer
--- train_occ_ray_ae.py
+++ train_occ_ray_ae.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 from tqdm import tqdm
 import torch
 from torch import nn
@@ -6,22 +7,27 @@ from torch.utils.data import DataLoader
 
 from lib.config.config import config
 from lib.logging.logging import log
-from lib.logging.tb import tb_writer
+from lib.logging.tb import get_tb_writer, initialize_tensorboard
 from lib.datasets.occ_ray_dataset import OccRayDataset
 from lib.models.occ_ray_ae import OccRayEncoder, OccRayDecoder
 
 
 BS = 16
-LR = 1e-3
-N_EPOCHS = 100
-N_BATCHES_LOG_INTERVAL = 64
+LR = 1e-4
+N_EPOCHS = 10000
+TRAINSET_LEN = 1024#*16
 RAY_RANGE = 1
-TRAINSET_LEN = 1024*16
+N_BATCHES_LOG_INTERVAL = TRAINSET_LEN//BS
 OCC_RAY_RESOLUTION = 45
-N_OCC_FCN_SAMPLES = 4
+N_OCC_FCN_SAMPLES = 16
 OCC_RAY_LATENT_DIM = 16
 
 def main():
+    if os.path.exists(config.exp_path):
+        shutil.rmtree(config.exp_path)
+    initialize_tensorboard()
+    tb_writer = get_tb_writer()
+
     train_dataset = OccRayDataset(
         range = RAY_RANGE,
         resolution = OCC_RAY_RESOLUTION,
@@ -57,7 +63,7 @@ def main():
 
     global_batch_cnt = 0
     for epoch in range(N_EPOCHS):
-        for batch in tqdm(train_dataloader):
+        for batch in tqdm(train_dataloader, 'Epoch #{}/{}'.format(epoch+1, N_EPOCHS)):
             z = occ_ray_encoder(batch['occ_ray_rasterized'].reshape((BS, 1, OCC_RAY_RESOLUTION)).cuda(), batch['grid'].reshape((BS, 1, OCC_RAY_RESOLUTION)).cuda())
             occ_fcn_vals_pred = occ_ray_decoder(
                 z.reshape((BS, 1, OCC_RAY_LATENT_DIM)).expand((-1, N_OCC_FCN_SAMPLES, -1)).reshape((BS*N_OCC_FCN_SAMPLES, OCC_RAY_LATENT_DIM)),
@@ -71,7 +77,7 @@ def main():
             if global_batch_cnt % N_BATCHES_LOG_INTERVAL == 0:
                 log.info('Train loss: {:.8f}'.format(loss.item()))
                 tb_writer.add_scalar("loss/train", loss, global_batch_cnt)
-                tb_writer.flush()
+                # tb_writer.flush()
             global_batch_cnt += 1
 
 if __name__ == '__main__':
