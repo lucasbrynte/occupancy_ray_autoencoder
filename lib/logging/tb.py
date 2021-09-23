988b394812b723bc09a2391a154d0daea2e87751
Lucas Brynte <brynte@chalmers.se>
2021-09-23 13:40:34 +0200

    minor name change tb_writer -> _tb_writer (variable should be private).

--- lib/logging/tb.py
+++ lib/logging/tb.py
@@ -1,16 +1,16 @@
 from torch.utils.tensorboard import SummaryWriter
 from lib.config.config import config
 
-global tb_writer
-tb_writer = None
+global _tb_writer
+_tb_writer = None
 
 def initialize_tensorboard():
-    global tb_writer
-    tb_writer = SummaryWriter(
+    global _tb_writer
+    _tb_writer = SummaryWriter(
         log_dir = config.tb_path,
         flush_secs = 30,
     )
-    return tb_writer
+    return _tb_writer
 
 def get_tb_writer():
-    return tb_writer
+    return _tb_writer
