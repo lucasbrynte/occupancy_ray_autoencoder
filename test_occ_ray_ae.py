a013bf7586b51073d0ae8393aaaee0a6572601e2
Lucas Brynte <brynte@chalmers.se>
2021-10-26 22:03:51 +0200

    Test visualization: latent code interpolation.

--- test_occ_ray_ae.py
+++ test_occ_ray_ae.py
@@ -69,6 +69,7 @@ def main():
     )
 
     with torch.no_grad():
+        global_pair_idx = 0
         for batch_idx, batch_data in enumerate(tqdm(test_dataloader_encoded_interpolation_samples, '[TEST INTERP ENC]')):
             # batch_data = preprocess_batch(batch_data) # add 'surface_pt_mask'
             z = occ_ray_encoder_forward(
@@ -86,6 +87,32 @@ def main():
             ).reshape((n_z_pairs, config.OCC_RAY_AE.TEST.DATA.INTERPOLATION_RESOLUTION, config.OCC_RAY_AE.N_DENSE_OCC_FCN_SAMPLES))
             # loss = calc_loss(dense_occ_fcn_vals_pred, batch_data['dense_occ_fcn_vals'])
 
+            for pair_idx in range(n_z_pairs):
+                idx_sample1 = 2*pair_idx
+                idx_sample2 = 2*pair_idx + 1
+                for interpol_idx, alpha in enumerate(range(config.OCC_RAY_AE.TEST.DATA.INTERPOLATION_RESOLUTION)):
+                    if interpol_idx == 0:
+                        occ_fcn_vals_target = batch_data['dense_occ_fcn_vals'][idx_sample1, :].numpy()
+                        occ_ray_rasterized = batch_data['occ_ray_rasterized'][idx_sample1, :].numpy()
+                    elif interpol_idx == config.OCC_RAY_AE.TEST.DATA.INTERPOLATION_RESOLUTION - 1:
+                        occ_fcn_vals_target = batch_data['dense_occ_fcn_vals'][idx_sample2, :].numpy()
+                        occ_ray_rasterized = batch_data['occ_ray_rasterized'][idx_sample2, :].numpy()
+                    else:
+                        occ_fcn_vals_target = None
+                        occ_ray_rasterized = None
+                    prediction_barplot(
+                        samples = batch_data['dense_pts'][idx_sample1, :].numpy(), # Effectively uses the samples of the first latent code of each pair.
+                        occ_fcn_vals_pred = dense_occ_fcn_vals_pred[pair_idx, interpol_idx, :].detach().cpu().numpy(),
+                        occ_fcn_vals_target = occ_fcn_vals_target,
+                        occ_ray_rasterized = occ_ray_rasterized,
+                        line_plot = True,
+                        write_tb = True,
+                        tb_tag = 'figures/interp_enc/test/pair_{:02d}'.format(global_pair_idx+1),
+                        tb_step = interpol_idx + 1,
+                    )
+                global_pair_idx += 1
+
+        global_pair_idx = 0
         for batch_idx, batch_data in enumerate(tqdm(test_dataloader_random_interpolation_samples, '[TEST INTERP RND]')):
             # batch_data = preprocess_batch(batch_data) # add 'surface_pt_mask'
             z_bs = batch_data['z'].shape[0]
@@ -97,3 +124,19 @@ def main():
                 batch_data['dense_pts'][::2, :].reshape((n_z_pairs, 1, config.OCC_RAY_AE.N_DENSE_OCC_FCN_SAMPLES)).expand(-1, config.OCC_RAY_AE.TEST.DATA.INTERPOLATION_RESOLUTION, -1).reshape((n_z_pairs * config.OCC_RAY_AE.TEST.DATA.INTERPOLATION_RESOLUTION, config.OCC_RAY_AE.N_DENSE_OCC_FCN_SAMPLES)),
                 z_interpol.reshape((n_z_pairs * config.OCC_RAY_AE.TEST.DATA.INTERPOLATION_RESOLUTION, config.OCC_RAY_AE.OCC_RAY_LATENT_DIM)),
             ).reshape((n_z_pairs, config.OCC_RAY_AE.TEST.DATA.INTERPOLATION_RESOLUTION, config.OCC_RAY_AE.N_DENSE_OCC_FCN_SAMPLES))
+
+            for pair_idx in range(n_z_pairs):
+                idx_sample1 = 2*pair_idx
+                idx_sample2 = 2*pair_idx + 1
+                for interpol_idx, alpha in enumerate(range(config.OCC_RAY_AE.TEST.DATA.INTERPOLATION_RESOLUTION)):
+                    prediction_barplot(
+                        samples = batch_data['dense_pts'][idx_sample1, :].numpy(), # Effectively uses the samples of the first latent code of each pair.
+                        occ_fcn_vals_pred = dense_occ_fcn_vals_pred[pair_idx, interpol_idx, :].detach().cpu().numpy(),
+                        # occ_fcn_vals_target = batch_data['dense_occ_fcn_vals'][pair_idx, interpol_idx, :].numpy(),
+                        # occ_ray_rasterized = batch_data['occ_ray_rasterized'][pair_idx, interpol_idx, :].numpy(),
+                        line_plot = True,
+                        write_tb = True,
+                        tb_tag = 'figures/interp_rnd/test/pair_{:02d}'.format(global_pair_idx+1),
+                        tb_step = interpol_idx + 1,
+                    )
+                global_pair_idx += 1
