866137f7d5138a81f608c00335b65b512c047a94
Lucas Brynte <brynte@chalmers.se>
2022-05-04 22:35:10 +0200

    Minor variable name refactoring

--- lib/loss/loss.py
+++ lib/loss/loss.py
@@ -19,11 +19,11 @@ def calc_loss(occ_fcn_vals_pred, occ_fcn_vals_target, point_weights=None):
         assert False
     return loss
 
-def calc_loss_anywhere_surface(batch_data, decoder_out):
+def calc_loss_anywhere_surface(batch_data, ae_out):
     # Note: Could be simplified significantly if first calculating loss on "anywhere" and "surface" separately, and then combining the losses.
     occ_fcn_vals_pred = torch.cat([
-        decoder_out['anywhere_occ_fcn_vals_pred'],
-        decoder_out['surface_occ_fcn_vals_pred'],
+        ae_out['anywhere_occ_fcn_vals_pred'],
+        ae_out['surface_occ_fcn_vals_pred'],
     ], dim=1).cuda()
     occ_fcn_vals_target = torch.cat([
         batch_data['anywhere_occ_fcn_vals'],
